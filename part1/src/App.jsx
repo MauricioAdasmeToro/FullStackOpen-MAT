a8725c7c2db9f2374fe7794994cecc8941f598e5
MauricioAdasmeToro <mauricio.adasmet@gmail.com>
2026-07-20 17:40:07 -0400

    Start to handle State in React by using the useState hook. The useState hook allows you to add state to functional components. In this example, we will create a simple counter application that increments a number each time a button is clicked.

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,71 +1,19 @@
 import { useState } from 'react'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.parts.name} {props.parts.exercises}
-      </p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part parts={props.parts[0]} />
-      <Part parts={props.parts[1]} />
-      <Part parts={props.parts[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
-
 const App = () => {
-    const course = {
-    name: 'Half Stack application development', 
-    parts: [
-    {
-    name: 'Fundamentals of React',
-    exercises: 10
-    },
-  
-   {
-    name: "Using props to pass data",
-    exercises: 7
-    },
-  
-    {
-    name:'State of a component',
-    exercises: 14
-    }
-   ]
-  }
-  
+  const  [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    console.log("Clicked");
+  };
+
+
+
   return (
       <div>
-        <Header course={course}/>
-        <Content 
-          parts={course.parts}
-        />
-        <Total parts={course.parts} />
-
-        
+        <div>{counter}</div>
+        <button onClick={handleClick}>plus</button>
       </div>
   )
 }
